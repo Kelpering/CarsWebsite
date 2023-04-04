0b88b8fd6759436448130fabfafc75c214d44903
kelpering <tristan8272@gmail.com>
2023-04-04 08:40:47 -0700

    Added Dealership Info

--- Backend/SearchPage.js
+++ Backend/SearchPage.js
@@ -3,15 +3,19 @@ $(function(){
     $(".car").hide();
     // let search = "Honda" // Change to receive input
     let search = sessionStorage.getItem("searchParam");
-    if (search == "" || search.length == 0 || search == " ")
+    if (search == "" || search == " " || search == null)
     {
         $(".car").show();
+    }
+    else
+    {
+        console.log(search);
     }
     let query = ".car:contains('" + search + "')"
     $(query).show();
 
     $(".params").text("Search params: " + search);
 
-    sessionStorage.setItem("searchParam", " ");
+    sessionStorage.setItem("searchParam", " "); // Clears search params after first page load
 
 })
