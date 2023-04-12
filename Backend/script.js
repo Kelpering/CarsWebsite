e00b979bf3805c5235bce30377572f21bf1b4d86
kelpering <tristan8272@gmail.com>
2023-04-12 09:02:58 -0700

    ICO Rotate

--- Backend/script.js
+++ Backend/script.js
@@ -1,3 +1,21 @@
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+var favicon = document.getElementById("favicon");
+var filept1 = "../ico/frame ("
+var filept2 = ").gif"
+async function start()
+{
+    while(true)
+    {
+        for (var i = 1; i < 172; i++)
+        {
+            favicon.href = filept1 + i + filept2;
+            await sleep(35);
+        }
+    }
+}
+
+
 $(function() {
 
     $(".searchBtn").click(function (e) { 
